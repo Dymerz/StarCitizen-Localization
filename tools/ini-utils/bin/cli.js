eb63110dd9c67313d4086e15f033ef804763ee82
Dymerz <21324342+Dymerz@users.noreply.github.com>
2025-05-19 19:35:48 +0200

    fix(cli): remove error handling for require in cli.js

--- tools/ini-utils/bin/cli.js
+++ tools/ini-utils/bin/cli.js
@@ -1,12 +1,4 @@
 #!/usr/bin/env node
 
 // This is a simple wrapper to ensure the compiled JavaScript version is executed
-try
-{
-  require('../dist/src/index.js');
-} catch (error)
-{
-  console.error('Error running ini-utils:', error.message);
-  console.log('Make sure you have built the project using "npm run build" before running.');
-  process.exit(1);
-}
+require('../dist/src/index.js');
